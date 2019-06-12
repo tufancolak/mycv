36ca5a24269c384e73226ef7c661271c7d7bff70
Tufan Colak <tufan@environment4change.com>
2019-06-12 17:20:24 +0100

    Layouts added and enabled.

---
+++ app/src/main/java/com/johndoe/mycv/screens/education/EducationActivity.kt
@@ -0,0 +1,13 @@
+package com.johndoe.mycv.screens.education
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.johndoe.mycv.R
+
+class EducationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_education)
+    }
+}
---
+++ app/src/main/java/com/johndoe/mycv/screens/work/WorkExperienceActivity.kt
@@ -0,0 +1,13 @@
+package com.johndoe.mycv.screens.work
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.johndoe.mycv.R
+
+class WorkExperienceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_work_experience)
+    }
+}
